214e46a824c168089a53c75f5a01783184e2f955
michael-wille-mscs <michael.wille@my.southeastmn.edu>
2017-09-26 00:42:03 -0500

    Added Ex2A1

---
+++ mwilleEx2A1/Main Form.vb
@@ -0,0 +1,103 @@
+﻿' Name:         Bakery Project
+' Purpose:      Calculates the total number of
+'               items sold and the total sales
+' Programmer:   Michael Wille on 9/25/2017
+
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Public Class frmMain
+
+
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        Const strPROMPT As String = "Salesclerk's name:"
+        Const strTITLE As String = "Name Entry"
+        Const decITEM_PRICE As Decimal = 0.5D
+        Const decTAX_RATE As Decimal = 0.02D
+        Dim intDonuts As Integer
+        Dim intMuffins As Integer
+        Dim intTotalItems As Integer
+        Dim decSubtotal As Decimal
+        Dim decSalesTax As Decimal
+        Dim decTotalSales As Decimal
+        Static strClerk As String
+
+        'Assign name to class-level variable
+        strClerk = InputBox(strPROMPT, strTITLE, strClerk)
+
+        'calculate total number of items sold
+        Integer.TryParse(txtDonuts.Text, intDonuts)
+        Integer.TryParse(txtMuffins.Text, intMuffins)
+        intTotalItems = intDonuts + intMuffins
+
+        'calculate the subtotal
+        decSubtotal = intTotalItems * decITEM_PRICE
+
+        'calculate the sales tax
+        decSalesTax = decSubtotal * decTAX_RATE
+
+        'calculate the total sales
+        decTotalSales = decSubtotal + decSalesTax
+
+        'display total amounts
+        lblTotalItems.Text = Convert.ToString(intTotalItems)
+        lblTotalSales.Text = decTotalSales.ToString("C2")
+
+        'display tax and salesclerk's name
+        lblMsg.Text = "The sales tax was " & decSalesTax.ToString("C2") & "." &
+            ControlChars.NewLine & strClerk
+
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' prepare screen for the next sale
+
+        txtDonuts.Text = String.Empty
+        txtMuffins.Text = String.Empty
+        lblTotalItems.Text = String.Empty
+        lblTotalSales.Text = String.Empty
+        lblMsg.Text = String.Empty
+        ' send the focus to the Doughnuts box
+        txtDonuts.Focus()
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'Get the salesclerk's name
+
+        'Const strPROMPT As String = "Salesclerk's name:"
+        'Const strTITLE As String = "Name Entry"
+        ''Assign name to class-level variable
+        'strClerk = InputBox(strPROMPT, strTITLE)
+    End Sub
+
+    Private Sub ClearLabels(sender As Object, e As EventArgs) _
+        Handles txtDonuts.TextChanged, txtMuffins.TextChanged
+        'clear the total items, total sales, and message
+        lblTotalItems.Text = String.Empty
+        lblTotalSales.Text = String.Empty
+        lblMsg.Text = String.Empty
+    End Sub
+
+    'Private Sub btnPrint_Click(sender As Object, e As EventArgs)
+    '    ' print the sales receipt
+
+    '    btnCalc.Visible = False
+    '    btnClear.Visible = False
+    '    btnExit.Visible = False
+    '    'btnPrint.Visible = False
+    '    'PrintForm1.Print()
+    '    btnCalc.Visible = True
+    '    btnClear.Visible = True
+    '    btnExit.Visible = True
+    '    'btnPrint.Visible = True
+
+    'End Sub
+End Class
