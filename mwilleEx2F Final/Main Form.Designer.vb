7b489e1e7df49838f0f841a711fd23c44e4d31c0
michael-wille-mscs <michael.wille@my.southeastmn.edu>
2017-10-18 04:07:06 -0500

    Added Ex2F Final Version

---
+++ mwilleEx2F Final/Main Form.Designer.vb
@@ -0,0 +1,421 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTotalDue = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblResortFee = New System.Windows.Forms.Label()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.lblTax = New System.Windows.Forms.Label()
+        Me.lblRoomChg = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtChildren = New System.Windows.Forms.TextBox()
+        Me.txtAdults = New System.Windows.Forms.TextBox()
+        Me.txtNights = New System.Windows.Forms.TextBox()
+        Me.txtRooms = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.lblParkingfee = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.radKing = New System.Windows.Forms.RadioButton()
+        Me.radQueen = New System.Windows.Forms.RadioButton()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.radAtrium = New System.Windows.Forms.RadioButton()
+        Me.radStandard = New System.Windows.Forms.RadioButton()
+        Me.chkParkingFee = New System.Windows.Forms.CheckBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblTotalDue
+        '
+        Me.lblTotalDue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotalDue.Location = New System.Drawing.Point(87, 186)
+        Me.lblTotalDue.Name = "lblTotalDue"
+        Me.lblTotalDue.Size = New System.Drawing.Size(93, 26)
+        Me.lblTotalDue.TabIndex = 9
+        Me.lblTotalDue.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(285, 397)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(97, 31)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblResortFee
+        '
+        Me.lblResortFee.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblResortFee.Location = New System.Drawing.Point(87, 124)
+        Me.lblResortFee.Name = "lblResortFee"
+        Me.lblResortFee.Size = New System.Drawing.Size(93, 26)
+        Me.lblResortFee.TabIndex = 7
+        Me.lblResortFee.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(150, 397)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(97, 31)
+        Me.btnCalc.TabIndex = 1
+        Me.btnCalc.Text = "Ca&lculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'lblTax
+        '
+        Me.lblTax.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTax.Location = New System.Drawing.Point(87, 93)
+        Me.lblTax.Name = "lblTax"
+        Me.lblTax.Size = New System.Drawing.Size(93, 26)
+        Me.lblTax.TabIndex = 6
+        Me.lblTax.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblRoomChg
+        '
+        Me.lblRoomChg.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblRoomChg.Location = New System.Drawing.Point(87, 62)
+        Me.lblRoomChg.Name = "lblRoomChg"
+        Me.lblRoomChg.Size = New System.Drawing.Size(93, 26)
+        Me.lblRoomChg.TabIndex = 5
+        Me.lblRoomChg.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtChildren)
+        Me.GroupBox1.Controls.Add(Me.txtAdults)
+        Me.GroupBox1.Controls.Add(Me.txtNights)
+        Me.GroupBox1.Controls.Add(Me.txtRooms)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(150, 176)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(217, 189)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Number of"
+        '
+        'txtChildren
+        '
+        Me.txtChildren.Location = New System.Drawing.Point(135, 129)
+        Me.txtChildren.Name = "txtChildren"
+        Me.txtChildren.Size = New System.Drawing.Size(40, 27)
+        Me.txtChildren.TabIndex = 7
+        '
+        'txtAdults
+        '
+        Me.txtAdults.Location = New System.Drawing.Point(21, 129)
+        Me.txtAdults.Name = "txtAdults"
+        Me.txtAdults.Size = New System.Drawing.Size(40, 27)
+        Me.txtAdults.TabIndex = 5
+        '
+        'txtNights
+        '
+        Me.txtNights.Location = New System.Drawing.Point(135, 61)
+        Me.txtNights.Name = "txtNights"
+        Me.txtNights.Size = New System.Drawing.Size(40, 27)
+        Me.txtNights.TabIndex = 3
+        '
+        'txtRooms
+        '
+        Me.txtRooms.Location = New System.Drawing.Point(21, 61)
+        Me.txtRooms.Name = "txtRooms"
+        Me.txtRooms.Size = New System.Drawing.Size(40, 27)
+        Me.txtRooms.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(131, 38)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(55, 20)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "&Nights:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(131, 106)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(67, 20)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "&Children:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(17, 106)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(94, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "&Adults (18+):"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(17, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "&Rooms:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(6, 186)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(75, 20)
+        Me.Label8.TabIndex = 4
+        Me.Label8.Text = "Total due:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(6, 124)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(79, 20)
+        Me.Label7.TabIndex = 2
+        Me.Label7.Text = "Resort fee:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(6, 62)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(52, 20)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "Room:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(6, 93)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(35, 20)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "Tax:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.lblParkingfee)
+        Me.GroupBox2.Controls.Add(Me.Label9)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.lblTotalDue)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.lblResortFee)
+        Me.GroupBox2.Controls.Add(Me.Label8)
+        Me.GroupBox2.Controls.Add(Me.lblRoomChg)
+        Me.GroupBox2.Controls.Add(Me.lblTax)
+        Me.GroupBox2.Location = New System.Drawing.Point(522, 178)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(201, 251)
+        Me.GroupBox2.TabIndex = 6
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Charges"
+        '
+        'lblParkingfee
+        '
+        Me.lblParkingfee.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblParkingfee.Location = New System.Drawing.Point(87, 155)
+        Me.lblParkingfee.Name = "lblParkingfee"
+        Me.lblParkingfee.Size = New System.Drawing.Size(93, 26)
+        Me.lblParkingfee.TabIndex = 8
+        Me.lblParkingfee.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(6, 155)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(61, 20)
+        Me.Label9.TabIndex = 3
+        Me.Label9.Text = "Parking:"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.radKing)
+        Me.GroupBox3.Controls.Add(Me.radQueen)
+        Me.GroupBox3.Location = New System.Drawing.Point(150, 23)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(147, 100)
+        Me.GroupBox3.TabIndex = 3
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Type"
+        '
+        'radKing
+        '
+        Me.radKing.AutoSize = True
+        Me.radKing.Location = New System.Drawing.Point(21, 57)
+        Me.radKing.Name = "radKing"
+        Me.radKing.Size = New System.Drawing.Size(86, 24)
+        Me.radKing.TabIndex = 1
+        Me.radKing.Text = "One &king"
+        Me.radKing.UseVisualStyleBackColor = True
+        '
+        'radQueen
+        '
+        Me.radQueen.AutoSize = True
+        Me.radQueen.Checked = True
+        Me.radQueen.Location = New System.Drawing.Point(21, 27)
+        Me.radQueen.Name = "radQueen"
+        Me.radQueen.Size = New System.Drawing.Size(100, 24)
+        Me.radQueen.TabIndex = 0
+        Me.radQueen.TabStop = True
+        Me.radQueen.Text = "Two &queen"
+        Me.radQueen.UseVisualStyleBackColor = True
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.radAtrium)
+        Me.GroupBox4.Controls.Add(Me.radStandard)
+        Me.GroupBox4.Location = New System.Drawing.Point(389, 23)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(147, 100)
+        Me.GroupBox4.TabIndex = 4
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "View"
+        '
+        'radAtrium
+        '
+        Me.radAtrium.AutoSize = True
+        Me.radAtrium.Location = New System.Drawing.Point(7, 57)
+        Me.radAtrium.Name = "radAtrium"
+        Me.radAtrium.Size = New System.Drawing.Size(72, 24)
+        Me.radAtrium.TabIndex = 1
+        Me.radAtrium.TabStop = True
+        Me.radAtrium.Text = "A&trium"
+        Me.radAtrium.UseVisualStyleBackColor = True
+        '
+        'radStandard
+        '
+        Me.radStandard.AutoSize = True
+        Me.radStandard.Checked = True
+        Me.radStandard.Location = New System.Drawing.Point(7, 27)
+        Me.radStandard.Name = "radStandard"
+        Me.radStandard.Size = New System.Drawing.Size(87, 24)
+        Me.radStandard.TabIndex = 0
+        Me.radStandard.TabStop = True
+        Me.radStandard.Text = "&Standard"
+        Me.radStandard.UseVisualStyleBackColor = True
+        '
+        'chkParkingFee
+        '
+        Me.chkParkingFee.AutoSize = True
+        Me.chkParkingFee.Location = New System.Drawing.Point(568, 50)
+        Me.chkParkingFee.Name = "chkParkingFee"
+        Me.chkParkingFee.Size = New System.Drawing.Size(155, 24)
+        Me.chkParkingFee.TabIndex = 5
+        Me.chkParkingFee.Text = "&Vehicle parking fee"
+        Me.chkParkingFee.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.mwille2F.My.Resources.Resources.Tree
+        Me.PictureBox1.Location = New System.Drawing.Point(21, 23)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(95, 135)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 22
+        Me.PictureBox1.TabStop = False
+        '
+        'frmMain
+        '
+        Me.AcceptButton = Me.btnCalc
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(735, 445)
+        Me.Controls.Add(Me.chkParkingFee)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.DoubleBuffered = True
+        Me.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "mwille Ex2F Treeline Resort"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblTotalDue As Label
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblResortFee As Label
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents lblTax As Label
+    Friend WithEvents lblRoomChg As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtChildren As TextBox
+    Friend WithEvents txtAdults As TextBox
+    Friend WithEvents txtNights As TextBox
+    Friend WithEvents txtRooms As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents lblParkingfee As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents radKing As RadioButton
+    Friend WithEvents radQueen As RadioButton
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents radStandard As RadioButton
+    Friend WithEvents radAtrium As RadioButton
+    Friend WithEvents chkParkingFee As CheckBox
+End Class
---
+++ mwilleEx2F Final/Main Form.vb
@@ -0,0 +1,134 @@
+﻿' Name:         Treeline Project
+' Purpose:      Display the total room charge, tax, 
+'               total resort fee, and total due
+' Programmer:   Michael Wille, 10/17/2017
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ClearLabels(sender As Object, e As EventArgs) _
+        Handles txtRooms.TextChanged, txtNights.TextChanged,
+        txtAdults.TextChanged, txtChildren.TextChanged, radQueen.CheckedChanged, radKing.CheckedChanged, radStandard.CheckedChanged, radAtrium.CheckedChanged, chkParkingFee.CheckedChanged
+        ' clear calculated amounts
+
+        lblRoomChg.Text = String.Empty
+        lblTax.Text = String.Empty
+        lblResortFee.Text = String.Empty
+        lblTotalDue.Text = String.Empty
+        lblParkingfee.Text = String.Empty
+
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        'calculate totals
+        Const intMAX_PER_ROOM As Integer = 6
+        Const dblDAILY_ROOM_CHG_QUEEN_STAND As Double = 225.5
+        Const dblDAILY_ROOM_CHG_QUEEN_ATRIUM As Double = 275
+        Const dblDAILY_ROOM_CHG_KING_STAND As Double = 245.5
+        Const dblDAILY_ROOM_CHG_KING_ATRIUM As Double = 325
+        Const dblDAILY_PARKING_FEE As Double = 8.5
+        Const dblTAX_RATE As Double = 0.1625
+        Const dblDAILY_RESORT_FEE As Double = 12.5
+        Const strMSG As String = "You have exceeded the maximum guests per room."
+        Dim intRoomsReserved As Integer
+        Dim intNights As Integer
+        Dim intAdults As Integer
+        Dim intChildren As Integer
+        Dim intNumGuests As Integer
+        Dim dblRoomsRequired As Double
+        Dim dblParkingFee As Double
+        Dim dblDailyRoomCharge As Double
+        Dim dblTotalRoomChg As Double
+        Dim dblTax As Double
+        Dim dblTotalResortFee As Double
+        Dim dblTotalDue As Double
+        Dim blnIsRoomReservedOK As Boolean
+        Dim blnIsNightsOK As Boolean
+        Dim blnIsAdultsOK As Boolean
+        'Dim blnIsChildrenOK As Boolean
+
+        blnIsRoomReservedOK = Integer.TryParse(txtRooms.Text, intRoomsReserved)
+        blnIsNightsOK = Integer.TryParse(txtNights.Text, intNights)
+        blnIsAdultsOK = Integer.TryParse(txtAdults.Text, intAdults)
+        Integer.TryParse(txtChildren.Text, intChildren)
+
+        If blnIsRoomReservedOK AndAlso blnIsNightsOK AndAlso blnIsAdultsOK Then
+
+
+            'number of guests
+            intNumGuests = intAdults + intChildren
+            'rooms needed for guests
+            dblRoomsRequired = intNumGuests / intMAX_PER_ROOM
+
+            If intRoomsReserved < dblRoomsRequired Then
+                MessageBox.Show(strMSG, "Treeline Resort", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Else
+                If radQueen.Checked Then
+                    If radStandard.Checked Then
+                        dblDailyRoomCharge = dblDAILY_ROOM_CHG_QUEEN_STAND
+                    Else
+                        dblDailyRoomCharge = dblDAILY_ROOM_CHG_QUEEN_ATRIUM
+                    End If
+                Else
+                    If radStandard.Checked Then
+                        dblDailyRoomCharge = dblDAILY_ROOM_CHG_KING_STAND
+                    Else
+                        dblDailyRoomCharge = dblDAILY_ROOM_CHG_KING_ATRIUM
+                    End If
+                End If
+                dblTotalRoomChg = intRoomsReserved * intNights * dblDailyRoomCharge
+                dblTax = dblTotalRoomChg * dblTAX_RATE
+                dblTotalResortFee = intRoomsReserved * intNights * dblDAILY_RESORT_FEE
+                If chkParkingFee.Checked Then
+                    dblParkingFee = intNights * dblDAILY_PARKING_FEE
+                End If
+                dblTotalDue = dblTotalRoomChg + dblTax + dblTotalResortFee + dblParkingFee
+
+                lblRoomChg.Text = dblTotalRoomChg.ToString("n2")
+                lblTax.Text = dblTax.ToString("n2")
+                lblResortFee.Text = dblTotalResortFee.ToString("n2")
+                lblParkingfee.Text = dblParkingFee.ToString("n2")
+                lblTotalDue.Text = dblTotalDue.ToString("n2")
+
+            End If
+        End If
+    End Sub
+
+    Private Sub CancelKeys_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtRooms.KeyPress, txtNights.KeyPress, txtAdults.KeyPress, txtChildren.KeyPress
+        'Text boxes only accept numbers or backspace
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso e.KeyChar <> ControlChars.Back Then
+            'cancel the entered key
+            e.Handled = True
+        End If
+
+
+
+    End Sub
+
+    Private Sub txtRooms_Enter(sender As Object, e As EventArgs) Handles txtRooms.Enter
+        'select all contents of the text box when it receives focus
+
+        txtRooms.SelectAll()
+
+    End Sub
+
+    Private Sub txtNights_Enter(sender As Object, e As EventArgs) Handles txtNights.Enter
+        txtNights.SelectAll()
+    End Sub
+
+    Private Sub txtAdults_Enter(sender As Object, e As EventArgs) Handles txtAdults.Enter
+        txtAdults.SelectAll()
+    End Sub
+
+    Private Sub txtChildren_Enter(sender As Object, e As EventArgs) Handles txtChildren.Enter
+        txtChildren.SelectAll()
+    End Sub
+
+
+End Class
