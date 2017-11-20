e1c46d5cd41c3c185fe5ce1bba66ba555fe65fba
michael-wille-mscs <31193373+michael-wille-mscs@users.noreply.github.com>
2017-11-19 21:03:17 -0600

    Added Ex3A1
    Ex3A1

---
+++ mwilleEx3A1/Form1.Designer.vb
@@ -0,0 +1,256 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnPretestDoWhile = New System.Windows.Forms.Button()
+        Me.btnPretestDoUntil = New System.Windows.Forms.Button()
+        Me.btnPosttestLoopWhile = New System.Windows.Forms.Button()
+        Me.btnPosttestLoopUntil = New System.Windows.Forms.Button()
+        Me.btnPretestFor = New System.Windows.Forms.Button()
+        Me.btnSumLoopUntil = New System.Windows.Forms.Button()
+        Me.btnSumDoWhile = New System.Windows.Forms.Button()
+        Me.btnSumForNext = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblEvenCountLoop = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.txtAddition = New System.Windows.Forms.TextBox()
+        Me.lblAdditionLoop = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblCount = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblAverage = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnPretestDoWhile
+        '
+        Me.btnPretestDoWhile.Location = New System.Drawing.Point(271, 73)
+        Me.btnPretestDoWhile.Name = "btnPretestDoWhile"
+        Me.btnPretestDoWhile.Size = New System.Drawing.Size(178, 47)
+        Me.btnPretestDoWhile.TabIndex = 0
+        Me.btnPretestDoWhile.Text = "Pretest: Do While"
+        Me.btnPretestDoWhile.UseVisualStyleBackColor = True
+        '
+        'btnPretestDoUntil
+        '
+        Me.btnPretestDoUntil.Location = New System.Drawing.Point(271, 142)
+        Me.btnPretestDoUntil.Name = "btnPretestDoUntil"
+        Me.btnPretestDoUntil.Size = New System.Drawing.Size(178, 47)
+        Me.btnPretestDoUntil.TabIndex = 1
+        Me.btnPretestDoUntil.Text = "Pretest: Do Until"
+        Me.btnPretestDoUntil.UseVisualStyleBackColor = True
+        '
+        'btnPosttestLoopWhile
+        '
+        Me.btnPosttestLoopWhile.Location = New System.Drawing.Point(271, 211)
+        Me.btnPosttestLoopWhile.Name = "btnPosttestLoopWhile"
+        Me.btnPosttestLoopWhile.Size = New System.Drawing.Size(178, 47)
+        Me.btnPosttestLoopWhile.TabIndex = 2
+        Me.btnPosttestLoopWhile.Text = "Posttest: Loop While"
+        Me.btnPosttestLoopWhile.UseVisualStyleBackColor = True
+        '
+        'btnPosttestLoopUntil
+        '
+        Me.btnPosttestLoopUntil.Location = New System.Drawing.Point(271, 280)
+        Me.btnPosttestLoopUntil.Name = "btnPosttestLoopUntil"
+        Me.btnPosttestLoopUntil.Size = New System.Drawing.Size(178, 47)
+        Me.btnPosttestLoopUntil.TabIndex = 3
+        Me.btnPosttestLoopUntil.Text = "Posttest: Loop Until"
+        Me.btnPosttestLoopUntil.UseVisualStyleBackColor = True
+        '
+        'btnPretestFor
+        '
+        Me.btnPretestFor.Location = New System.Drawing.Point(271, 349)
+        Me.btnPretestFor.Name = "btnPretestFor"
+        Me.btnPretestFor.Size = New System.Drawing.Size(178, 47)
+        Me.btnPretestFor.TabIndex = 4
+        Me.btnPretestFor.Text = "Pretest: For"
+        Me.btnPretestFor.UseVisualStyleBackColor = True
+        '
+        'btnSumLoopUntil
+        '
+        Me.btnSumLoopUntil.Location = New System.Drawing.Point(285, 189)
+        Me.btnSumLoopUntil.Name = "btnSumLoopUntil"
+        Me.btnSumLoopUntil.Size = New System.Drawing.Size(178, 47)
+        Me.btnSumLoopUntil.TabIndex = 5
+        Me.btnSumLoopUntil.Text = "Sum: Loop Until"
+        Me.btnSumLoopUntil.UseVisualStyleBackColor = True
+        '
+        'btnSumDoWhile
+        '
+        Me.btnSumDoWhile.Location = New System.Drawing.Point(285, 136)
+        Me.btnSumDoWhile.Name = "btnSumDoWhile"
+        Me.btnSumDoWhile.Size = New System.Drawing.Size(178, 47)
+        Me.btnSumDoWhile.TabIndex = 6
+        Me.btnSumDoWhile.Text = "Sum: Do While"
+        Me.btnSumDoWhile.UseVisualStyleBackColor = True
+        '
+        'btnSumForNext
+        '
+        Me.btnSumForNext.Location = New System.Drawing.Point(285, 242)
+        Me.btnSumForNext.Name = "btnSumForNext"
+        Me.btnSumForNext.Size = New System.Drawing.Size(178, 47)
+        Me.btnSumForNext.TabIndex = 7
+        Me.btnSumForNext.Text = "Sum: For Next"
+        Me.btnSumForNext.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblEvenCountLoop)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 28)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(455, 420)
+        Me.GroupBox1.TabIndex = 8
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "p347 #13 Loop 2 - 20"
+        '
+        'lblEvenCountLoop
+        '
+        Me.lblEvenCountLoop.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblEvenCountLoop.Location = New System.Drawing.Point(7, 45)
+        Me.lblEvenCountLoop.Name = "lblEvenCountLoop"
+        Me.lblEvenCountLoop.Size = New System.Drawing.Size(246, 372)
+        Me.lblEvenCountLoop.TabIndex = 0
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.txtAddition)
+        Me.GroupBox2.Controls.Add(Me.lblAdditionLoop)
+        Me.GroupBox2.Controls.Add(Me.btnSumDoWhile)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.lblCount)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Controls.Add(Me.btnSumLoopUntil)
+        Me.GroupBox2.Controls.Add(Me.lblAverage)
+        Me.GroupBox2.Controls.Add(Me.btnSumForNext)
+        Me.GroupBox2.Location = New System.Drawing.Point(515, 28)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(488, 420)
+        Me.GroupBox2.TabIndex = 9
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "p332_348 #22 Addition"
+        '
+        'txtAddition
+        '
+        Me.txtAddition.Location = New System.Drawing.Point(7, 45)
+        Me.txtAddition.Multiline = True
+        Me.txtAddition.Name = "txtAddition"
+        Me.txtAddition.Size = New System.Drawing.Size(259, 372)
+        Me.txtAddition.TabIndex = 14
+        '
+        'lblAdditionLoop
+        '
+        Me.lblAdditionLoop.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblAdditionLoop.Location = New System.Drawing.Point(6, 45)
+        Me.lblAdditionLoop.Name = "lblAdditionLoop"
+        Me.lblAdditionLoop.Size = New System.Drawing.Size(260, 372)
+        Me.lblAdditionLoop.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(369, 15)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 17)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Count:"
+        '
+        'lblCount
+        '
+        Me.lblCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblCount.Location = New System.Drawing.Point(372, 45)
+        Me.lblCount.Name = "lblCount"
+        Me.lblCount.Size = New System.Drawing.Size(91, 47)
+        Me.lblCount.TabIndex = 13
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(269, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 17)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Average:"
+        '
+        'lblAverage
+        '
+        Me.lblAverage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblAverage.Location = New System.Drawing.Point(272, 45)
+        Me.lblAverage.Name = "lblAverage"
+        Me.lblAverage.Size = New System.Drawing.Size(91, 47)
+        Me.lblAverage.TabIndex = 12
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(788, 454)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(178, 47)
+        Me.btnExit.TabIndex = 14
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1015, 508)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnPretestDoWhile)
+        Me.Controls.Add(Me.btnPosttestLoopWhile)
+        Me.Controls.Add(Me.btnPretestDoUntil)
+        Me.Controls.Add(Me.btnPosttestLoopUntil)
+        Me.Controls.Add(Me.btnPretestFor)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "frmMain"
+        Me.Text = "mwilleEx3A1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnPretestDoWhile As Button
+    Friend WithEvents btnPretestDoUntil As Button
+    Friend WithEvents btnPosttestLoopWhile As Button
+    Friend WithEvents btnPosttestLoopUntil As Button
+    Friend WithEvents btnPretestFor As Button
+    Friend WithEvents btnSumLoopUntil As Button
+    Friend WithEvents btnSumDoWhile As Button
+    Friend WithEvents btnSumForNext As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblAverage As Label
+    Friend WithEvents lblCount As Label
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblEvenCountLoop As Label
+    Friend WithEvents lblAdditionLoop As Label
+    Friend WithEvents txtAddition As TextBox
+End Class
---
+++ mwilleEx3A1/Form1.vb
@@ -0,0 +1,204 @@
+﻿'Programmer: Michael Wille
+'Purpose: Test different loop types
+'Date Created: 11/16/2017
+
+Option Strict On
+Option Explicit On
+Option Infer Off
+
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnPretestDoWhile_Click(sender As Object, e As EventArgs) Handles btnPretestDoWhile.Click
+        lblEvenCountLoop.Text = "Pretest Do While" & ControlChars.NewLine
+        Dim i As Integer = 2
+        Do While i <= 20
+            lblEvenCountLoop.Text = lblEvenCountLoop.Text & i.ToString & ControlChars.NewLine
+            i += 2
+        Loop
+    End Sub
+
+    Private Sub btnPretestDoUntil_Click(sender As Object, e As EventArgs) Handles btnPretestDoUntil.Click
+        lblEvenCountLoop.Text = "Pretest Do Until" & ControlChars.NewLine
+        Dim i As Integer = 2
+        Do Until i > 20
+            lblEvenCountLoop.Text = lblEvenCountLoop.Text & i.ToString & ControlChars.NewLine
+            i += 2
+        Loop
+    End Sub
+
+    Private Sub btnPosttestLoopWhile_Click(sender As Object, e As EventArgs) Handles btnPosttestLoopWhile.Click
+        lblEvenCountLoop.Text = "Posttest Loop While" & ControlChars.NewLine
+        Dim i As Integer = 2
+        Do
+            lblEvenCountLoop.Text = lblEvenCountLoop.Text & i.ToString & ControlChars.NewLine
+            i += 2
+        Loop While i <= 20
+    End Sub
+
+    Private Sub btnPosttestLoopUntil_Click(sender As Object, e As EventArgs) Handles btnPosttestLoopUntil.Click
+        lblEvenCountLoop.Text = "Posttest Loop Until" & ControlChars.NewLine
+        Dim i As Integer = 2
+        Do
+            lblEvenCountLoop.Text = lblEvenCountLoop.Text & i.ToString & ControlChars.NewLine
+            i += 2
+        Loop Until i > 20
+    End Sub
+
+    Private Sub btnPretestFor_Click(sender As Object, e As EventArgs) Handles btnPretestFor.Click
+        lblEvenCountLoop.Text = "Pretest For" & ControlChars.NewLine
+
+        For i As Integer = 2 To 20 Step 2
+            lblEvenCountLoop.Text = lblEvenCountLoop.Text & i.ToString & ControlChars.NewLine
+        Next i
+
+
+    End Sub
+
+    Private Sub btnSumDoWhile_Click(sender As Object, e As EventArgs) Handles btnSumDoWhile.Click
+        txtAddition.Text = String.Empty
+        lblCount.Text = String.Empty
+        lblAverage.Text = String.Empty
+
+        Const strPROMPT As String =
+            "Enter a number. " & ControlChars.NewLine & "Click cancel or leave black to the end."
+        Const strTITLE As String = "Number Entry"
+        Dim strNUM As String
+        Dim intNUM As Integer
+        Dim intSUM As Integer
+        Dim intCOUNT As Integer
+        Dim dblAVG As Double
+
+        strNUM = InputBox(strPROMPT, strTITLE, "0")
+        'begin loop here: repeat as long as input is not empty
+        Do While Int32.TryParse(strNUM, intNUM)
+
+
+            txtAddition.Text = txtAddition.Text & intNUM.ToString & ControlChars.NewLine
+
+
+            'update the counter and accumulator
+            intCOUNT = intCOUNT + 1
+            intSUM = intSUM + intNUM
+
+            'Update Control Variable
+            strNUM = InputBox(strPROMPT, strTITLE, "0")
+            ' End Loop Here
+        Loop
+
+        lblCount.Text = intCOUNT.ToString
+
+        'Check count for being greater than 0
+        If intCOUNT > 0 Then
+            dblAVG = intSUM / intCOUNT
+            lblAverage.Text = dblAVG.ToString("N2")
+
+        Else
+            lblAverage.Text = "N/A"
+
+        End If
+
+
+    End Sub
+
+    Private Sub btnSumLoopUntil_Click(sender As Object, e As EventArgs) Handles btnSumLoopUntil.Click
+        txtAddition.Text = String.Empty
+        lblCount.Text = String.Empty
+        lblAverage.Text = String.Empty
+
+        Const strPROMPT As String = "Enter a number. " & ControlChars.NewLine & "Click cancel or leave black to the end."
+        Const strTITLE As String = "Number Entry"
+        Dim strNUM As String
+        Dim intNUM As Integer
+        Dim intSUM As Integer
+        Dim intCOUNT As Integer
+        Dim dblAVG As Double
+
+        strNUM = InputBox(strPROMPT, strTITLE, "0")
+        'begin loop here: repeat as long as input is not empty
+        Do
+            Int32.TryParse(strNUM, intNUM)
+            txtAddition.Text = txtAddition.Text & intNUM.ToString & ControlChars.NewLine
+
+
+            'update the counter and accumulator
+            intCOUNT = intCOUNT + 1
+            intSUM = intSUM + intNUM
+
+            'Update Control Variable
+            strNUM = InputBox(strPROMPT, strTITLE, "0")
+            ' End Loop Here
+        Loop Until CInt(Int32.TryParse(strNUM, intNUM)) = 0
+
+        lblCount.Text = intCOUNT.ToString
+
+        'Check count for being greater than 0
+        If intCOUNT > 0 Then
+            dblAVG = intSUM / intCOUNT
+            lblAverage.Text = dblAVG.ToString("N2")
+
+        Else
+            lblAverage.Text = "N/A"
+
+        End If
+
+
+
+    End Sub
+
+    Private Sub btnSumForNext_Click(sender As Object, e As EventArgs) Handles btnSumForNext.Click
+        txtAddition.Text = String.Empty
+        lblCount.Text = String.Empty
+        lblAverage.Text = String.Empty
+
+        Const strPROMPT As String = "Enter the amount of numbers to be process. " & ControlChars.NewLine
+        Const strPROMPT2 As String = "Enter Value"
+        Const strTITLE As String = "Enter Number"
+        Const strTITLE2 As String = "Enter Value"
+        Dim strNUMBER As Integer
+        Dim strNUM As String
+        Dim intNUM As Integer
+        Dim intSUM As Integer
+        Dim intCOUNT As Integer
+        Dim dblAVG As Double
+
+        strNUM = InputBox(strPROMPT, strTITLE, "0")
+        Int32.TryParse(strNUM, intNUM)
+        'begin loop here: repeat as long as input is not empty
+        For quantity As Integer = intNUM To 1 Step -1
+
+            'step prompt 2 foward by one
+            strNUMBER = CInt((" " & strNUMBER + 1).ToString)
+
+            'Update Control Variable
+            'If strPROMPT2 =
+            strNUM = InputBox(strPROMPT2 & " " & "#" & strNUMBER, strTITLE2, "0") 'Then
+            Int32.TryParse(strNUM, intNUM)
+                txtAddition.Text = txtAddition.Text & intNUM.ToString & ControlChars.NewLine
+            'End If
+            'update the counter and accumulator
+            intCOUNT = intCOUNT + 1
+            intSUM = intSUM + intNUM
+
+
+            ' End Loop Here
+        Next quantity
+
+        lblCount.Text = intCOUNT.ToString
+
+        'Check count for being greater than 0
+        If intCOUNT > 0 Then
+            dblAVG = intSUM / intCOUNT
+            lblAverage.Text = dblAVG.ToString("N2")
+
+        Else
+            lblAverage.Text = "N/A"
+
+        End If
+
+
+    End Sub
+
+End Class
